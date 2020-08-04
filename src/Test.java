877122d740732a7f70dad0dc7d7035d6c0422278
felixalle <davic2007@qq.com>
2020-08-04 19:20:01 +0800

    Update for the first time

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("这是第一个为GitHub准备的Java项目");
+        System.out.println("修改第一次：2020/08/04 19:19");
     }
 }
